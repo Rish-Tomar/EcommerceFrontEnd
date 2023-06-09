6091bb166d1099136e18802ed9691fab8e2b145f
Rish_Tomar <rishabh.tomar2@gmail.com>
2023-06-09 20:36:06 +0530

    React router added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,38 @@ import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home/>),
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage/>,
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      {/* <LoginPage/> */}
+      {/* <SignupPage/> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
