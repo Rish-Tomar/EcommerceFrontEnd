7fc00f6c824fdb4d4bed34151b01705c91330d6c
Rish_Tomar <rishabh.tomar2@gmail.com>
2023-06-09 21:05:12 +0530

    Cart Component and routes for cart added

--- src/features/navbar/NavBar.js
+++ src/features/navbar/NavBar.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon,ShoppingCartIcon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
 
 const user = {
   name: 'Tom Cook',
@@ -63,12 +64,14 @@ export default function NavBar({children}) {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
+                        <Link to='/cart'>
                       <button
                         type="button"
                         className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                       >
                         <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
                       </button>
+                      </Link>
                       <span className="inline-flex items-center rounded-md mb-5 -ml-3 bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
                             3
                         </span>
@@ -151,12 +154,14 @@ export default function NavBar({children}) {
                       <div className="text-base font-medium leading-none text-white">{user.name}</div>
                       <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
                     </div>
+                    <Link to='/cart'>
                     <button
                       type="button"
                       className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                     >
                       <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
+                    </Link>
                     <span className="inline-flex items-center rounded-md mb-5 -ml-3 bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
                         2
                     </span>
