3cc234cdfb3300b3d9c99dcac9e9c8360cae4bef
Rish_Tomar <rishabh.tomar2@gmail.com>
2023-06-13 06:49:52 +0530

    sorting category filters added

---
+++ src/features/productList/dataConstants.js
@@ -0,0 +1,59 @@
+export const Brandoptions=[
+    {
+        checked: false,    label: "Apple",    value: "Apple"
+      }, {
+        checked: false,    label: "Samsung",    value: "Samsung"
+      }, {
+        checked: false,    label: "OPPO",    value: "OPPO"
+      }, {
+        checked: false,    label: "Huawei",    value: "Huawei"
+      }, {
+        checked: false,    label: "Microsoft Surface",    value: "Microsoft Surface"
+      }, {
+        checked: false,    label: "Infinix",    value: "Infinix" 
+     }, {
+        checked: false,    label: "HP Pavilion",    value: "HP Pavilion"
+      }, {
+        checked: false,    label: "Impression of Acqua Di Gio",    value: "Impression of Acqua Di Gio"
+      }, {
+        checked: false,    label: "Royal_Mirage",    value: "Royal_Mirage"
+      }, {
+        checked: false,    label: "Fog Scent Xpressio",    value: "Fog Scent Xpressio"
+      }, {
+        checked: false,    label: "Al Munakh",    value: "Al Munakh"
+      }, {
+        checked: false,    label: "Lord   Al Rehab",    value: "Lord - Al-Rehab"
+      }, {
+        checked: false,
+        label: "L'Oreal Paris",
+        value: "L'Oreal Paris"
+      }, {
+        checked: false,
+        label: "Hemani Tea",
+        value: "Hemani Tea"
+      }, {
+        checked: false,
+        label: "Dermive",
+        value: "Dermive"
+      }, {
+        checked: false,
+        label: "ROREC White Rice",
+        value: "ROREC White Rice"
+      }, {
+        checked: false,
+        label: "Fair &amp; Clear",
+        value: "Fair &amp; Clear"
+      }, {
+        checked: false,
+        label: "Saaf &amp; Khaas",
+        value: "Saaf &amp; Khaas"
+      }, {
+        checked: false,
+        label: "Bake Parlor Big",
+        value: "Bake Parlor Big"
+      }, {
+        checked: false,
+        label: "Baking Food Items",
+        value: "Baking Food Items"
+      }
+]
--- src/features/productList/productListAPI.js
+++ src/features/productList/productListAPI.js
@@ -1,7 +1,23 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async(resolve) =>
-   { const response=await fetch('http://localhost:8002')
+  //  todo
+   { const response=await fetch('http://localhost:3000/products')
+    const data =await response.json()
+    resolve({data})}
+  );
+}
+
+export function fetchProductsByFilters(filter) {
+  //filter has object with key values like filter={"category":"laptop"}
+
+  let querryString="";
+  for (let key in filter){
+    querryString+=`${key}=${filter[key]}&`
+  }
+  return new Promise(async(resolve) =>
+  //  todo
+   { const response=await fetch('http://localhost:3000/products?'+querryString)
     const data =await response.json()
     resolve({data})}
   );
